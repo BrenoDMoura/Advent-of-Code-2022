9962068558c3bf5ef6d9b8f2ccc0f8b3bd2ff4b6
BrenoDMoura <brenobatista1990@gmail.com>
2023-09-01 10:07:15 -0300

    Optmizing Solutions

--- Day01/Day01.py
+++ Day01/Day01.py
@@ -5,24 +5,25 @@ elfs = []
 max = 0
 calories = []
 
-#FILTRANDO \n:
+#REMOVING \n:
 
 for e in range(len(txt)):
     txt[e] = txt[e][:-1]
     
     if(txt[e] != ""):
         txt[e] = int(txt[e])
-        #ATÉ AQUI FUNCIONA!
         max += txt[e]
     else:
         calories.append(max)
         max = 0
         pass
-    #ATÉ AQUI, O CÓDIGO EXIBE NA LISTA LUCENA,
-    #QUANTAS CALORIAS OS ELFOS POSSUEM 
+    
 
 #SOLUTION FOR PART 1:
-bigger = 0
+#bigger = 0
+
+calories.sort(reverse=True)
+print(calories[0]) #71471
 
 # for e in range(len(calories)):
 #     if(calories[e] > bigger):
@@ -31,11 +32,8 @@ bigger = 0
 #print(bigger)  71471
 #####################
 
-##### PART 2 SOLUTION #####
-calories.sort(reverse=True)
-for e in range(3):
-    bigger += calories[e]
-
-print(bigger)
+##### PART 2 SOLUTION ##### 211189
+#
+#print(sum(calories[:3]))
 ###########################
 
